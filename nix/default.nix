6d90437c9b6b43a7f62bc89c8e20f8f54d3838dc
pixel <edwardoliverthomas@gmail.com>
2025-11-22 15:32:15 +0000

    fix: update vendorHash and main program name in default.nix

--- nix/default.nix
+++ nix/default.nix
@@ -8,7 +8,9 @@ buildGoModule {
 
   src = ../src;
 
-  vendorHash = null;
+  vendorHash = "sha256-XWsBc8QKelO0FRwr2QgSNTNHyY9pD6fr1karRDc1CtY=";
+
+  subPackages = [ "cmd/api" ];
 
   ldflags = [
     "-s"
@@ -16,11 +18,15 @@ buildGoModule {
     "-X main.version=${version}"
   ];
 
+  postInstall = ''
+    mv $out/bin/api $out/bin/duo-streak-widget
+  '';
+
   meta = {
     description = "A duolingo 88x31 button";
     homepage = "https://github.com/pixel-87/duo-streak-widget";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ ];
-    mainProgram = "example";
+    mainProgram = "duo-streak-widget";
   };
 }
