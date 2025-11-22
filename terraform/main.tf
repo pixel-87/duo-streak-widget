b1b5296efd0be2b80ed45ea4238e2f3bb228d782
pixel <edwardoliverthomas@gmail.com>
2025-11-22 15:44:36 +0000

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "duo-streak-widget"
+  region  = "europe-west2"
+}
+
+resource "google_cloud_run_v2_service" "default" {
+  name     = "duo-streak-widget"
+  location = "europe-west2"
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "gcr.io/duo-streak-widget/duo-streak-widget:latest"
+      ports {
+        container_port = 8080
+      }
+      env {
+        name  = "PORT"
+        value = "8080"
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_member" "noauth" {
+  location = google_cloud_run_v2_service.default.location
+  name     = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
